da409684bfe0540752a0d64c5525f755e1cc8184
unknown <rahulroutara@gmail.com>
2022-11-20 13:33:37 +0530

    first commit

---
+++ src/db/connection.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const DB = process.env.DATABASE;
+//making connection::
+mongoose.connect(DB, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    // useCreateIndex: true
+}).then(()=>{
+    console.log(`connection successful`);
+}).catch(err => console.log(err));
